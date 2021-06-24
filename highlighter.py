9180c58e81159f43a6d6c622724f4fb7d56cd2a5
Prerak Lodha <57831679+prerakl123@users.noreply.github.com>
2021-06-24 15:10:00 +0530

    Add files via upload

--- highlighter.py
+++ highlighter.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog
+from tkinter import font as tk_font
+from tkinter import ttk
+from tkinter import colorchooser
 import re
 import inspect
 import os
 
-font = ('Consolas', 11)
+font = ['Consolas', 11]
 label_frame_config = {
     'bg': '#000033',
     'fg': '#d6d6d6'
@@ -28,6 +31,39 @@ text_config = {
 button_config = {
     'bg': '#040050',
     'fg': '#ffffff'
+}
+label_config = {
+    'bg': '#040052',
+    'fg': '#a8a8a8',
+    'font': font
+}
+checkbtn_config = {
+    'relief': GROOVE,
+    'onvalue': 1,
+    'offvalue': 0,
+    'bg': '#040052',
+    'fg': '#a8a8a8',
+    'anchor': W
+}
+combo_config = {
+    'selectbackground': '#264180',
+    'selectforeground': 'white',
+    # 'background': '#040052',
+    'foreground': '#a8a8a8',
+    'fieldbackground': '#040052',
+    'insertcolor': 'white',
+    'insertwidth': 1,
+    'highlightthickness': 1,
+    'highlightbackground': 'white',
+}
+entry_config = {
+    'bg': '#040052',
+    'fg': '#a8a8a8',
+    'selectforeground': 'white',
+    'selectbackground': '#264180',
+    'insertwidth': 1,
+    'insertbackground': 'white',
+    'font': font
 }
 
 
@@ -103,9 +139,11 @@ class ScrollableFrame(LabelFrame):
 
 
 class RegexFrame(Frame):
-    def __init__(self, master, updatefunc, regex='', description='', value='', **kw):
+    def __init__(self, master, updatefunc, regex='', description='', value='', config_args='', **kw):
         Frame.__init__(self, master, **kw)
         self.config(highlightthickness=1, highlightbackground='black', **frame_config)
+
+        self.configuration_dict = dict()
 
         self.value_entry = Text(self, font=font, height=4, width=20, **text_config)
         self.regex_entry = Text(self, font=font, height=4, width=20, **text_config)
@@ -123,6 +161,14 @@ class RegexFrame(Frame):
         # self.description_entry.bind('<FocusOut>', updatefunc)
         # self.regex_entry.bind('<FocusOut>', updatefunc)
         self.bind('<FocusOut>', updatefunc)
+        self.arg_configuration(config_args)
+        print(self.configuration_dict)
+
+    def arg_configuration(self, config_args=''):
+        config_args = config_args.split('|')
+        for expression in config_args:
+            exp_list = expression.split('=')
+            self.configuration_dict[exp_list[0]] = int(exp_list[1]) if exp_list[1].isnumeric() else str(exp_list[1])
 
 
 class RegexEditingFrame(LabelFrame):
@@ -138,11 +184,92 @@ class RegexEditingFrame(LabelFrame):
         self.text.pack(expand=True, fill=BOTH, padx=1, pady=1)
 
 
+class RegexConfigEditingFrame(LabelFrame):
+    def __init__(self, master, frame, configurations: dict, **kw):
+        LabelFrame.__init__(self, master, **kw)
+
+        self.bold_intvar = IntVar()
+        self.italic_intvar = IntVar()
+        self.underline_intvar = IntVar()
+        self.overstrike_intvar = IntVar()
+
+        combostyle = ttk.Style()
+
+        combostyle.theme_create('combostyle', parent='alt', settings={'TCombobox': {'configure': combo_config}})
+        combostyle.theme_use('combostyle')
+
+        self.choose_font_combo = ttk.Combobox(self, text='Choose Font', font=font)
+        self.choose_font_combo.option_add('*TCombobox*Listbox.background', '#040052')
+        self.choose_font_combo.option_add('*TCombobox*Listbox.font', 'Consolas 9')
+        self.choose_font_combo.option_add('*TCombobox*Listbox.foreground', '#a8a8a8')
+        self.choose_font_combo.option_add('*TCombobox*Listbox.selectBackground', '#264180')
+        self.choose_font_combo.option_add('*TCombobox*Listbox.selectForeground', 'white')
+        self.font_size_lbl = Label(self, text='Font Size', **label_config, width=18)
+        self.font_size_entry = Entry(self, width=4, **entry_config)
+        self.bold_checkbtn = Checkbutton(self, text='Bold', font=font.__add__(['bold']), **checkbtn_config,
+                                         variable=self.bold_intvar, width=15)
+        self.italic_checkbtn = Checkbutton(self, text='Italic', font=font.__add__(['italic']), **checkbtn_config,
+                                           variable=self.italic_intvar, width=15)
+        self.underline_checkbtn = Checkbutton(self, text='Underline', font=font.__add__(['underline']),
+                                              **checkbtn_config, variable=self.underline_intvar, width=15)
+        self.overstrike_checkbtn = Checkbutton(self, text='Overstrike', variable=self.overstrike_intvar,
+                                               **checkbtn_config, font=font.__add__(['overstrike']), width=15)
+        self.fg_color_lbl = Label(self, text='Foreground Color: ', **label_config)
+        self.fg_color = Label(self, text='    ', **label_config)
+        self.fg_color.bind('<Enter>', lambda _=None: self.fg_color.config(cursor='hand2'))
+        self.fg_color.bind('<Leave>', lambda _=None: self.fg_color.config(cursor=None))
+        self.fg_color.bind('<ButtonRelease-1>', lambda _=None: self.show_color_palette(self.fg_color))
+        self.bg_color_lbl = Label(self, text='Background Color: ', **label_config)
+        self.bg_color = Label(self, text='    ', **label_config)
+        self.bg_color.bind('<Enter>', lambda _=None: self.bg_color.config(cursor='hand2'))
+        self.bg_color.bind('<Leave>', lambda _=None: self.bg_color.config(cursor=None))
+        self.bg_color.bind('<ButtonRelease-1>', lambda _=None: self.show_color_palette(self.bg_color))
+
+        self.choose_font_combo.grid(row=0, column=0, sticky=NSEW, columnspan=3)
+        self.font_size_lbl.grid(row=1, column=0, sticky=W, padx=3)
+        self.font_size_entry.grid(row=1, column=1, sticky=W, padx=3)
+        self.bold_checkbtn.grid(row=2, column=0, sticky=W, padx=3)
+        self.italic_checkbtn.grid(row=3, column=0, sticky=W, padx=3)
+        self.underline_checkbtn.grid(row=3, column=1, sticky=W, padx=3)
+        self.overstrike_checkbtn.grid(row=2, column=1, sticky=W, padx=3)
+        self.fg_color_lbl.grid(row=6, column=0, sticky=W, padx=3)
+        self.fg_color.grid(row=6, column=1, sticky=W, padx=3)
+        self.bg_color_lbl.grid(row=7, column=0, sticky=W, padx=3)
+        self.bg_color.grid(row=7, column=1, sticky=W, padx=3)
+
+        font_names = sorted(tk_font.families())
+        self.choose_font_combo['values'] = font_names
+
+        self.configure_configurations(configurations)
+
+    def configure_configurations(self, configurations: dict):
+        self.font_size_entry.insert(0, configurations.get('size'))
+        self.choose_font_combo.set(value=configurations.get('family'))
+        self.bold_intvar.set(configurations.get('bold'))
+        self.italic_intvar.set(configurations.get('italic'))
+        self.underline_intvar.set(configurations.get('underline'))
+        self.overstrike_intvar.set(configurations.get('overstrike'))
+        self.bg_color.config(bg=configurations.get('bg'))
+        self.fg_color.config(bg=configurations.get('fg'))
+
+    def show_color_palette(self, label):
+        color = colorchooser.askcolor(label['background'])
+        if color == (None, None):
+            pass
+        else:
+            label.config(bg=color[-1])
+
+
 class RegexTextArea(ScrolledText):
     TAB_LENGTH = 4
 
-    def __init__(self, master, **kw):
+    def __init__(self, master, value_list, regex_list, regex_config_args: dict, **kw):
         ScrolledText.__init__(self, master, **kw)
+
+        self.value_list = value_list
+        self.regex_list = regex_list
+        self.regex_config_args = regex_config_args
+        self.textfilter = None
 
         self.custom_tags = ['active_line', 'similar_selection']
 
@@ -160,11 +287,44 @@ class RegexTextArea(ScrolledText):
         self.bind('<KeyPress-quotedbl>', lambda a: self.autocomplete(val='"'))
         self.bind('<Return>', self.on_return)
 
+        self.config_regex_args()
         self.highlight_current_line()
+        self.apply_regex()
 
     def _any(self, name, alternates):
         """Return a named group pattern matching list of alternates."""
         return "(?P<%s>" % name + "|".join(alternates) + ")"
+
+    def _coordinate(self, start, end, string):
+        """Returns indices of the start and end of matched `string`"""
+        srow = string[:start].count('\n') + 1
+        scolsplitlines = string[:start].split('\n')
+
+        if len(scolsplitlines) != 0:
+            scolsplitlines = scolsplitlines[len(scolsplitlines) - 1]
+
+        scol = len(scolsplitlines)
+        lrow = string[:end + 1].count('\n') + 1
+        lcolsplitlines = string[:end].split('\n')
+
+        if len(lcolsplitlines) != 0:
+            lcolsplitlines = lcolsplitlines[len(lcolsplitlines) - 1]
+
+        lcol = len(lcolsplitlines) + 1
+
+        return '{}.{}'.format(srow, scol), '{}.{}'.format(lrow, lcol)
+
+    def apply_regex(self):
+        self.textfilter = re.compile(self.create_regex(), re.S)
+
+    def create_regex(self):
+        regex_list = []
+        for regex, value in zip(self.regex_list, self.value_list):
+            regex_list.append(self._any(value, regex))
+        return '|'.join(regex_list)
+
+    def config_regex_args(self):
+        pass
 
     def autocomplete(self, val):
         if val == '(':
@@ -423,10 +583,9 @@ class RegexTextArea(ScrolledText):
             self.after(25, self.toggle_highlight)
 
     def refresh(self, value_list, regex_list):
-        pass
-
-    def set_values(self, *values):
-        pass
+        self.value_list = value_list
+        self.regex_list = regex_list
+        self.apply_regex()
 
 
 class RegexAndDescriptionWindow(Toplevel):
@@ -468,6 +627,14 @@ class RegexAndDescriptionWindow(Toplevel):
         self.destroy()
 
 
+class RegexConfigurationWindow(Toplevel):
+    def __init__(self, master, frame_list, value_list, **kw):
+        Toplevel.__init__(self, master, **kw)
+
+    def _insert_config_frames(self):
+        pass
+
+
 class Window(Tk):
     VALUE_LIST = []
     REGEX_LIST = []
@@ -491,7 +658,9 @@ class Window(Tk):
                               relief=GROOVE, **button_config)
         self.open_file_btn = Button(self.buttons_frame, text='Open File', font=font, command=self._open_file,
                                     relief=GROOVE, **button_config)
-        self.text_area = RegexTextArea(self.text_area_frame, font=font, wrap=NONE, **text_config)
+        self.text_area = RegexTextArea(self.text_area_frame, value_list=self.VALUE_LIST, regex_list=self.REGEX_LIST,
+                                       regex_config_args=self._extract_regex_config_args(), font=font, wrap=NONE,
+                                       **text_config)
 
         # --------------------- PACKING WIDGETS --------------------- #
         self.buttons_frame.pack(side=TOP, anchor=W, fill=X, padx=2, pady=2)
@@ -525,8 +694,8 @@ class Window(Tk):
         with open('TEXT-History', 'r') as text_file:
             self.text_area.insert(1.0, text_file.read().rstrip())
 
-    def add_new_regex_frame(self, event=None, regex='', description='', value=''):
-        rf = RegexFrame(self.widget_frame, self._update, regex, description, value)
+    def add_new_regex_frame(self, event=None, regex='', description='', value='', config_args=''):
+        rf = RegexFrame(self.widget_frame, self._update, regex, description, value, config_args)
         rf.pack(side=TOP, fill=X, expand=True, pady=1)
         rf.regex_entry.bind('<ButtonRelease-3>', self._r_click)
         rf.description_entry.bind('<ButtonRelease-3>', self._r_click)
@@ -547,10 +716,20 @@ class Window(Tk):
 
     def set_regex_values(self, *args):
         """
-        args must be lists like ['<REGEX1>', 'DESCRIPTION1', 'VALUE1'], ['<REGEX2>', 'DESCRIPTION2', 'VALUE2'], etc
+        args must be lists like ['<REGEX1>', 'DESCRIPTION1', 'VALUE1' [, 'italic'|'bold', etc]],
+        ['<REGEX2>', 'DESCRIPTION2', 'VALUE2' [, 'strikethrough'|'bold', etc], etc
         """
-        for reg, desc, val in args:
-            self.add_new_regex_frame(None, reg, desc, val)
+        try:
+            for reg, desc, val, config_args in args:
+                self.add_new_regex_frame(None, reg, desc, val, config_args)
+        except ValueError:
+            pass
+
+    def _extract_regex_config_args(self) -> dict:
+        return_dict = dict()
+        for frame, value in zip(self.FRAME_LIST, self.VALUE_LIST):
+            return_dict[value] = frame.configuration_dict
+        return return_dict
 
     def _open_file(self):
         file = filedialog.askopenfile(initialdir=os.path.dirname(os.path.abspath(inspect.getsourcefile(self.__class__))
@@ -570,14 +749,18 @@ class Window(Tk):
             description = True
 
         r_click_menu = Menu(self, tearoff=0)
-        r_click_menu.add_command(label='Clear', command=lambda: event.widget.event_generate('<<Clear>>'))
         r_click_menu.add_command(label='Copy', command=lambda: event.widget.event_generate('<<Copy>>'))
         r_click_menu.add_command(label='Cut', command=lambda: event.widget.event_generate('<<Cut>>'))
-        r_click_menu.add_command(label='Edit in Separate Window', command=lambda: RegexAndDescriptionWindow(
-            self, self.FRAME_LIST, self.VALUE_LIST, description))
         r_click_menu.add_command(label='Paste', command=lambda: event.widget.event_generate('<<Paste>>'))
+        r_click_menu.add_separator()
+        r_click_menu.add_command(label='Clear', command=lambda: event.widget.delete(1.0, END))
         r_click_menu.add_command(label='Remove', command=lambda: remove(event))
         r_click_menu.add_command(label='Select All', command=lambda: event.widget.event_generate('<<SelectAll>>'))
+        r_click_menu.add_separator()
+        r_click_menu.add_command(label='Edit in Separate Window', command=lambda: RegexAndDescriptionWindow(
+            self, self.FRAME_LIST, self.VALUE_LIST, description))
+        r_click_menu.add_command(label='Configure Appearance', command=lambda: RegexConfigurationWindow(
+            self, self.FRAME_LIST, self.VALUE_LIST))
         r_click_menu.tk_popup(event.x_root, event.y_root, 0)
         r_click_menu.grab_release()
 
@@ -596,6 +779,12 @@ class Window(Tk):
             line_elements = [frame.regex_entry.get(1.0, END).rstrip('\n').rstrip(),
                              frame.description_entry.get(1.0, END).rstrip('\n').rstrip(),
                              frame.value_entry.get(1.0, END).rstrip('\n').rstrip()]
+            config_dict_str = ''
+            for tup in list(frame.configuration_dict.items()):
+                config_dict_str += '='.join([tup[0], str(tup[1])]) + '|'
+            config_dict_str = config_dict_str.rstrip('|')
+            line_elements.append(config_dict_str)
+
             if '' in line_elements:
                 continue
             line = '<===>'.join(line_elements)
